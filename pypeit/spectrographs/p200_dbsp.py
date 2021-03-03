f8cb9c3f2aa41f94bfa406b3bc72fd327addd056
Milan Roberson <finagle29@gmail.com>
2021-03-03 00:37:41 -0800

    grow DBSPr BPM

--- pypeit/spectrographs/p200_dbsp.py
+++ pypeit/spectrographs/p200_dbsp.py
@@ -503,6 +503,6 @@ class P200DBSPRedSpectrograph(P200DBSPSpectrograph):
         if msbias is not None:
             return self.bpm_frombias(msbias, det, bpm_img)
 
-        bpm_img[463:723, :] = 1
+        bpm_img[460:723, :] = 1
 
         return bpm_img
